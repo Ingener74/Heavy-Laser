51086b20d609017c781c2b1dc959b1c84dd1ee7b
Shnaider Pavel <shnaiderpasha@gmail.com>
2019-04-26 13:45:41 +0500

    Сделал значок в трее и запуск из трея приложение работает всё время

--- Heavy-Laser.py
+++ Heavy-Laser.py
@@ -1,8 +1,9 @@
 import sys
 
-from PySide2.QtCore import QTime, QTimer, Qt, Signal
-from PySide2.QtGui import QPainter, QColor, QPaintEvent, QKeyEvent
-from PySide2.QtWidgets import QApplication, QWidget
+import PySide2
+from PySide2.QtCore import QTime, QTimer, Qt, Signal, Slot
+from PySide2.QtGui import QPainter, QColor, QPaintEvent, QKeyEvent, QPixmap
+from PySide2.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QMenu, QAction
 
 from blackclockwidget import Ui_BlackClock
 
@@ -16,9 +17,8 @@ class MainWidget(QWidget):
         self.ui.setupUi(self)
 
         pal = self.ui.lcdNumber.palette()
-
         pal.setColor(pal.WindowText, Qt.gray)
-
+        pal.setColor(pal.Background, Qt.red)
         self.ui.lcdNumber.setPalette(pal)
 
         self.timer1 = QTimer(self)
@@ -37,23 +37,69 @@ class MainWidget(QWidget):
         if event.key() == Qt.Key_Escape:
             self.close_widget.emit()
 
+    def resizeEvent(self, event: PySide2.QtGui.QResizeEvent):
+        super().resizeEvent(event)
+        self.ui.lcdNumber.setMinimumWidth(self.width() / 4)
+        self.ui.lcdNumber.setMinimumHeight(self.height() / 4)
+        self.ui.lcdNumber.move(self.width() / 2 - self.ui.lcdNumber.width() / 2,
+                               self.height() / 2 - self.ui.lcdNumber.height() / 2)
+
     def show_time(self):
         self.ui.lcdNumber.display(QTime.currentTime().toString("hh:mm"))
 
 
+class Windows(object):
+    def __init__(self):
+        self.wins = []
+        desktop = QApplication.desktop()
+        for i in range(desktop.screenCount()):
+            w = MainWidget()
+            geometry = desktop.screenGeometry(i)
+            w.setGeometry(geometry)
+            w.setWindowFlags(Qt.FramelessWindowHint)
+            # w.showFullScreen()
+            w.close_widget.connect(self.close_all)
+            self.wins.append(w)
+
+    @Slot()
+    def show_all(self):
+        for w in self.wins:
+            w.showFullScreen()
+
+    @Slot()
+    def close_all(self):
+        for w in self.wins:
+            w.close()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    QApplication.setQuitOnLastWindowClosed(False)
 
-    wins = []
-    desktop = QApplication.desktop()
-    for i in range(desktop.screenCount()):
-        w = MainWidget()
-        geometry = desktop.screenGeometry(i)
-        w.setGeometry(geometry)
-        w.setWindowFlags(Qt.FramelessWindowHint)
-        w.showFullScreen()
-        w.close_widget.connect(app.quit)
-        # w.show()
-        wins.append(w)
+    wins = Windows()
+
+    menu = QMenu()
+
+    make_screen_black_action = QAction(QPixmap(':/start.png'), 'Make Screen Black')
+    make_screen_black_action.triggered.connect(wins.show_all)
+
+    def message():
+        system_tray.showMessage('Test message', 'Foo is 42', QPixmap(':/main.png'))
+
+    message_action = QAction('Message')
+    message_action.triggered.connect(message)
+
+    quit_action = QAction('Quit')
+    quit_action.triggered.connect(app.quit)
+
+    menu.addAction(make_screen_black_action)
+    menu.addAction(message_action)
+    menu.addSeparator()
+    menu.addAction(quit_action)
+
+    system_tray = QSystemTrayIcon()
+    system_tray.setContextMenu(menu)
+    system_tray.setIcon(QPixmap(':/main.png'))
+    system_tray.show()
 
     sys.exit(app.exec_())
--- blackclockwidget.py
+++ blackclockwidget.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'blackclockwidget.ui',
 # licensing of 'blackclockwidget.ui' applies.
 #
-# Created: Tue Apr  9 22:17:34 2019
+# Created: Tue Apr 23 16:51:52 2019
 #      by: pyside2-uic  running on PySide2 5.12.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,12 +13,12 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_BlackClock(object):
     def setupUi(self, BlackClock):
         BlackClock.setObjectName("BlackClock")
-        BlackClock.resize(1412, 1075)
+        BlackClock.resize(1032, 648)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/main/laser_pointer_256.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/main.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         BlackClock.setWindowIcon(icon)
         self.lcdNumber = QtWidgets.QLCDNumber(BlackClock)
-        self.lcdNumber.setGeometry(QtCore.QRect(30, 30, 1241, 901))
+        self.lcdNumber.setGeometry(QtCore.QRect(30, 30, 851, 521))
         self.lcdNumber.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.lcdNumber.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
         self.lcdNumber.setObjectName("lcdNumber")
